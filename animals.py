3e8871817271a6a694efbf773ef284b08e862754
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2022-10-28 09:06:26 -0600

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,4 @@
+animals = ["shih tzu", "malimute", "beagle", "goldendoodle", "pug", "huskey"]
+for animal in animals:
+        print(f"A {animal} is a type of dog.")
+print("All of these dog breeds are beautiful, and make great pets.")
---
+++ pizza.py
@@ -0,0 +1,4 @@
+pizzas = ["hawaiian", "spinech and feta", "cheese", "vegetarian"]
+for pizza in pizzas:
+        print(f"I like {pizza} pizza.")
+print("There is so much variety in pizza, it's hard to choose just one as a favorite.")
